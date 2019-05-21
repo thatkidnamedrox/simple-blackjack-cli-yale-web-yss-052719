2082be65879e60fa561275aadcd1646fe39a99e4
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-21 18:41:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,29 +1,40 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table."
 end
 
 def deal_card
   # code #deal_card here
+  number = rand(2..10)
+  return number
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}."
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay."
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  number_1 = deal_card
+  number_2 = deal_card
+  card_total = number_1 + number_2
+  display_card_total(card_total)
 end
 
 def hit?
@@ -41,4 +52,3 @@ end
 def runner
   # code runner here
 end
-    
